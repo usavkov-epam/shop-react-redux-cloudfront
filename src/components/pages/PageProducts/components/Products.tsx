fb1683dbc1378fbc2287ae79a7b063bf5941a997
Yury Saukou <yury_saukou@epam.com>
2022-06-06 20:51:19 +0400

    Connect /products with BE

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,4 +1,4 @@
-// import axios from 'axios';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 import Card from '@material-ui/core/Card';
@@ -10,10 +10,9 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { AddProductToCart } from "../../../../components";
-// import { API_PATHS } from "../../../../constants";
+import { API_PATHS } from "../../../../constants";
 import { Product } from "../../../../models/Product";
 import { formatAsPrice } from "../../../../utils";
-import productList from "./productList.json";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -38,9 +37,8 @@ export function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    // axios.get(`${API_PATHS.bff}/product/available/`)
-    //   .then(res => setProducts(res.data));
-    setProducts(productList);
+    axios.get(`${API_PATHS.bff}/products`)
+      .then(res => setProducts(res.data));
   }, [])
 
   return (
