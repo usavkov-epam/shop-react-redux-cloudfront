7498e9323e75c5b171352012cd6ff6bd47d0e6a0
Yury Saukou <yury_saukou@epam.com>
2022-06-26 19:43:07 +0400

    Connect /products with BE (#3)

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -114,7 +114,7 @@ export default function PageProductForm() {
   const onSubmit = (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
     const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
-    axios.put(`${API_PATHS.bff}/product`, productToSave)
+    axios.put(`${API_PATHS.bff}/products`, productToSave)
       .then(() => navigate('/admin/products'));
   };
 
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,6 +3,6 @@ export const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://t554pox8g5.execute-api.eu-west-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
