d859d8644dfc6a1af3e80cf7f1c9f0206d9f1ad0
Yury Saukou <yury_saukou@epam.com>
2022-07-10 13:10:38 +0400

    Integrate UI with S3 import service (#5)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 export const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://pkd0uy3q04.execute-api.eu-west-1.amazonaws.com',
   bff: 'https://t554pox8g5.execute-api.eu-west-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
