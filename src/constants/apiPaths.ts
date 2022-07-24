9741f41618bd589d27acae1caefa73be0efc9ac7
Yury Saukou <yury_saukou@epam.com>
2022-07-25 00:26:35 +0400

    Update import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 export const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://pkd0uy3q04.execute-api.eu-west-1.amazonaws.com',
+  import: 'https://f6ng5ipg3h.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://t554pox8g5.execute-api.eu-west-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
