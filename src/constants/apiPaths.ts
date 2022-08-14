24bbb3179d8ce36aeaf5c4b5f710086fa9c49c36
Yury Saukou <yury_saukou@epam.com>
2022-08-14 13:57:05 +0400

    Update UI to support cart service API (#7)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,5 +4,5 @@ export const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://f6ng5ipg3h.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://t554pox8g5.execute-api.eu-west-1.amazonaws.com',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://usavkov-epam-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
